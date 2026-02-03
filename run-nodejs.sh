8b7789e952c38b5e384ff96aa6c775d809ec8a48
Kunal Kapoor <kksworld@gmail.com>
2026-02-02 18:27:02 -0800

    Display node process id and port

--- run-nodejs.sh
+++ run-nodejs.sh
@@ -2,22 +2,26 @@
 
 sudo apt update
 
-echo "Installing nodejs..."
-sudo apt install nodejs npm -y
+#Install packages
+echo "Installing..."
+sudo apt install nodejs npm curl net-tools -y
 
+#Display Node JS version
 echo "Node JS version: $(node -v)"
 echo
 
+#Display NPM version
 echo "NPM version: $(npm -v)"
 echo
 
+#Download NodeJS project
 echo "Downloading node JS project tar..."
 sudo wget https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz
 
 echo "Extracting tar file.."
 sudo tar -xvzf bootcamp-node-envvars-project-1.0.0.tgz
 
-
+#Set environment variables
 echo "Setting environment variables..."
 
 ENV_FILE="$HOME/.app_env"
@@ -33,13 +37,35 @@ EOF
 
 cd package
 
+#install npm
 echo
 echo "Installing npm dependencies..."
 npm install
 echo
 
 
+#Kill if node is already running
+if [ -f server.pid ]; then
+  kill "$(cat server.pid)" || true
+fi
+
+#Start Node JS
 echo "Running Node JS application..."
 node server.js > server.log 2>&1 &
+echo $! > server.pid
 
 
+# Display that nodejs process is running
+echo
+
+echo "Node processes running:"
+pgrep -u "$USER" -l node
+
+sleep 1
+
+#PORT=$(sudo /usr/sbin/netstat -plnt 2>/dev/null | grep node | awk '{print $4}' | awk -F':' '{print $NF}')
+#echo "Node is listening on port: $PORT"
+
+#Get NodeJS port
+port=$(sudo netstat -pl|grep node|awk '{print $4}'|awk -F':' '{print $4}')
+echo "Node is listening on port: $port"
